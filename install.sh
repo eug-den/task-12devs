e9d9911d3b3e86eb4ae33a67881d6571fd87313a
eug-den <eug_den@tut.by>
2023-04-03 19:08:25 +0300

    install.sh delay added

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # start LEMP
 docker compose -f ./docker/docker-compose.yaml up -d
 
+# wait for LEMP initializing
+SLEEP_TIME=10s
+echo -e "\nWait $SLEEP_TIME \bec for LEMP initializing ...\n"
+sleep "$SLEEP_TIME"
+
 # create bases, tables & fill them with random data
 cp ./php/install.php ./html/install.php
 docker exec -it php php /var/www/html/install.php
